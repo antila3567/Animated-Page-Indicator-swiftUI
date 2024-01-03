79b75507a27321c099988dfd356ef65a59fb2f35
IvanL <antila3567@gmail.com>
2024-01-03 16:38:20 +0200

    Initial Commit

---
+++ AnimatedPageIndicator/AnimatedPageIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedPageIndicatorApp.swift
+//  AnimatedPageIndicator
+//
+//  Created by Иван Легенький on 03.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedPageIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
